4f8a6eba9f58b927e3a75051d9b324e4162116e0
marybngozi <umeh.mary@appmartgroup.com>
2022-02-07 22:36:11 +0100

    add dog single view page

--- src/views/Dog.vue
+++ src/views/Dog.vue
@@ -5,8 +5,34 @@
     <!-- clear the fixed effect on header -->
     <div class="h-20"></div>
 
-    <main class="mt-3">
-      <p>{{ dog }}</p>
+    <main class="mt-3 w-11/12 mx-auto">
+      <div class="flex justify-between items-center mb-4">
+        <router-link
+          aria-roledescription="back button"
+          to="/"
+          class="border py-3 px-4 rounded border-brown"
+        >
+          <img
+            src="@/assets/img/arrow_back.svg"
+            alt="Back icon"
+            class="w-3 mr-2"
+          />
+          <span>Back</span>
+        </router-link>
+
+        <h3 class="uppercase fs-4">{{ dog.author }}</h3>
+      </div>
+
+      <section class="w-11/12 md:w-8/12 mx-auto mb-10">
+        <figure class="w-full flex flex-col justify-center">
+          <img :src="dog.image" alt="dog.author" />
+          <figcaption class="sr-only">
+            {{ dog.author }}
+          </figcaption>
+        </figure>
+        <h4 class="py-4 text-brown"><b>Genre: </b>{{ dog.genre }}</h4>
+        <p class="fs-6 text-justify">{{ dog.content }}</p>
+      </section>
     </main>
   </div>
 </template>
@@ -43,3 +69,17 @@ export default {
   },
 };
 </script>
+<style scoped>
+a {
+  text-decoration: none;
+  @apply text-brown font-bold;
+}
+figure {
+  height: auto;
+}
+figure img {
+  width: 100%;
+  height: auto;
+  /* max-height: 100%; */
+}
+</style>
